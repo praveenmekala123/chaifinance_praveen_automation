a135c32be2c151718051d2ac4a0a2ea2a0352e78
Praveen <praveen.crystal@gmail.com>
2022-10-31 18:01:26 +0530

    Uncommented some wanted code

--- PaymentFlowAutomation/src/test/java/PaymentFlowAutomation/testcases/CheckoutPageTest.java
+++ PaymentFlowAutomation/src/test/java/PaymentFlowAutomation/testcases/CheckoutPageTest.java
@@ -32,14 +32,14 @@ public class CheckoutPageTest extends TestBase {
 	}
 
 	//Verify Pay Now by selecting PayNow from wallets
-//	@Test
-//	public void selectPayNowWallet() throws InterruptedException {
-//		checkoutPage.selectWallets();
-//		checkoutPage.payUsingPayNowWallet();
-//		checkoutPage.clickOnPayNow();
-//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-//		System.out.println(driver.getTitle());
-//	}
+	@Test
+	public void selectPayNowWallet() throws InterruptedException {
+		checkoutPage.selectWallets();
+		checkoutPage.payUsingPayNowWallet();
+		checkoutPage.clickOnPayNow();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		System.out.println(driver.getTitle());
+	}
 
 	//Verify Pay Now by selecting Atome option from BNPL
 	@Test
@@ -53,47 +53,47 @@ public class CheckoutPageTest extends TestBase {
 	}
 
 	//Verify checkout amount is same as in the payment page
-//	@Test
-//	public void validateCheckoutAmount() throws InterruptedException {
-//		String checkoutAmt = "";
-//
-//		Thread.sleep(3000);
-//		System.out.println(checkoutPage.getAmount());
-//		String[] checkoutAmts = (checkoutPage.getAmount().split(" ")[1]).split("\\.");
-//		if(checkoutAmts.length > 1 && String.valueOf(Math.round(Float.valueOf(checkoutAmts[1]))).equals("0")) {
-//			checkoutAmt = checkoutAmts[0];
-//		}else  {
-//			checkoutAmt = checkoutPage.getAmount().split(" ")[1];
-//		}
-//
-//		Assert.assertEquals(checkoutAmt, defAmount);
-//	}
+	@Test
+	public void validateCheckoutAmount() throws InterruptedException {
+		String checkoutAmt = "";
+
+		Thread.sleep(3000);
+		System.out.println(checkoutPage.getAmount());
+		String[] checkoutAmts = (checkoutPage.getAmount().split(" ")[1]).split("\\.");
+		if(checkoutAmts.length > 1 && String.valueOf(Math.round(Float.valueOf(checkoutAmts[1]))).equals("0")) {
+			checkoutAmt = checkoutAmts[0];
+		}else  {
+			checkoutAmt = checkoutPage.getAmount().split(" ")[1];
+		}
+
+		Assert.assertEquals(checkoutAmt, defAmount);
+	}
 
 	//Verify Pay Now button by not selecting any payment option
-//	@Test
-//	public void payNowWithoutPaymentOption() {
-//		String defAlertMsg = "Please select a payment option!";
-//		checkoutPage.clickOnPayNow();
-//		String invalidAlertMsg = checkoutPage.showAlert();
-//		Assert.assertEquals(invalidAlertMsg, defAlertMsg);
-//	}
+	@Test
+	public void payNowWithoutPaymentOption() {
+		String defAlertMsg = "Please select a payment option!";
+		checkoutPage.clickOnPayNow();
+		String invalidAlertMsg = checkoutPage.showAlert();
+		Assert.assertEquals(invalidAlertMsg, defAlertMsg);
+	}
 
 	//Verify Pay Now button by not entering credit card number completely
-//	@Test
-//	public void enterCreditcardNumb() throws InterruptedException {
-//
-//		String defPaymentFailedAlert = "Payment Failed!";
-//		checkoutPage.selectCreditCard();
-//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
-//		checkoutPage.clickOnCardNumb("1111");
-//		checkoutPage.clickOnPayNow();
-//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
-//		String payFailAlrt = checkoutPage.showPaymentFailedAlert();
-//		Assert.assertEquals(payFailAlrt, defPaymentFailedAlert);
-//	}
+	@Test
+	public void enterCreditcardNumb() throws InterruptedException {
 
-//	@AfterMethod
-//	public void closeBrowser() {
-//		driver.quit();
-//	}
+		String defPaymentFailedAlert = "Payment Failed!";
+		checkoutPage.selectCreditCard();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		checkoutPage.clickOnCardNumb("1111");
+		checkoutPage.clickOnPayNow();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		String payFailAlrt = checkoutPage.showPaymentFailedAlert();
+		Assert.assertEquals(payFailAlrt, defPaymentFailedAlert);
+	}
+
+	@AfterMethod
+	public void closeBrowser() {
+		driver.quit();
+	}
 }
